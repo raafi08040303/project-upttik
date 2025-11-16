3568d1e581e783ebb2b7c7f907cce2a886c985c4
Rahadian Dwi Putra <rahadian.dwiputra@ai.astra.co.id>
2025-11-16 23:17:36 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return "Halo Admin!";
+})->middleware(['auth', 'role:admin']);
+
+Route::get('/staff', function () {
+    return "Halo Staff!";
+})->middleware(['auth', 'role:staff']);
+
+Route::get('/user', function () {
+    return "Halo User!";
+})->middleware(['auth', 'role:user']);
